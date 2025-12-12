2bf04f6a1ad6ee749958eb88a6b787fb71bcebfd
sanbo <sanbo.xyz@gmail.com>
2025-12-12 19:59:08 +0800

    update sth at 2025-12-12 19:59:08

--- getproxy/cli.py
+++ getproxy/cli.py
@@ -8,17 +8,10 @@ from getproxy import GetProxy
 @click.option('--in-proxy', help='Input proxy file')
 @click.option('--out-proxy', help='Output proxy file')
 @click.option('--token', help='github token')
-def main(in_proxy, out_proxy):
-    g = GetProxy(in_proxy, out_proxy)
+def main(in_proxy, out_proxy, token):
+    g = GetProxy(in_proxy, out_proxy, token)
     g.start()
 
-@click.command()
-@click.option('--in-proxy', help='Input proxy file')
-@click.option('--out-proxy', help='Output proxy file')
-@click.option('--token', help='github token')
-def main(in_proxy, out_proxy, token):
-    g = GetProxy(in_proxy, out_proxy,token)
-    g.start()
 
 if __name__ == "__main__":
     main()
